2c635cce3632a94389c75563f4c40bdc36eb6644
Renat Zubairov <renat.zubairov@sopera.de>
2010-05-17 23:18:31 +0200

    Added an empty application using phonegap empty template

---
+++ IPhonePhotoApp/Classes/IPhonePhotoAppAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  IPhonePhotoAppAppDelegate.h
+//  IPhonePhotoApp
+//
+//  Created by Renat Zubairov on 17.05.10.
+//  Copyright Sopera GmbH 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface IPhonePhotoAppAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
